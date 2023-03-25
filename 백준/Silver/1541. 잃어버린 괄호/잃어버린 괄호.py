f0c18b36a834b4350a26afb7d198c1d7e5d3774f
rihyun1126 <49039762+rihyun1126@users.noreply.github.com>
2023-03-25 23:14:11 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -1,28 +1,18 @@
-s = input()
-
-num = ''
-minus = 0
+expression = list(input().split('-'))
 answer = 0
 
-for i in range(len(s)):
-    if s[i] == '-':
-        if minus == 0:
-            answer += int(num)
-        else:
-            answer -= int(num)
-        num = ''
-        minus += 1
-    elif s[i] == '+':
-        if minus == 0:
-            answer += int(num)
-        else:
-            answer -= int(num)
-        num = ''
-    else:
-        num += s[i]
+def plus(l):
+    nums = list(l.split('+'))
+    sum = 0
+    for i in nums:
+        sum += int(i)
+    return sum
 
-if minus != 0:
-    answer -= int(num)
-else:
-    answer += int(num)
+for i in range(len(expression)):
+    num = plus(expression[i])
+    if i == 0:
+        answer += num
+    else:
+        answer -= num
+
 print(answer)
