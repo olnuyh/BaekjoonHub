694e02f76858ce9d8ad53c9d465f7dbf31ed5ecd
rihyun1126 <49039762+rihyun1126@users.noreply.github.com>
2023-04-06 16:49:40 +0900

    [Silver III] Title: 문자열 집합, Time: 116 ms, Memory: 36808 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.py
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -1,50 +1,13 @@
 import sys
 read = sys.stdin.readline
 
-class Node(object):
-    def __init__(self, isEnd):
-        self.isEnd = isEnd
-        self.childNode = {}
-
-class Trie(object):
-    def __init__(self):
-        self.parent = Node(None)
-
-    def insert(self, string):
-        nowNode = self.parent
-        length = 0
-        for char in string:
-            if char not in nowNode.childNode:
-                nowNode.childNode[char] = Node(char)
-            nowNode = nowNode.childNode[char]
-            length += 1
-            if length == len(string):
-                nowNode.isEnd = True
-
-    def search(self, string):
-        nowNode = self.parent
-        length = 0
-        for char in string:
-            if char in nowNode.childNode:
-                nowNode = nowNode.childNode[char]
-                length += 1
-                if length == len(string) and nowNode.isEnd == True:
-                    return True
-            else:
-                return False
-        return False
-
 n, m = map(int, read().split())
-trie = Trie()
-
-for _ in range(n):
-    word = read().strip()
-    trie.insert(word)
-
+strList = set([read() for _ in range(n)])
 answer = 0
+
 for _ in range(m):
-    word = read().strip()
-    if trie.search(word):
+    word = read()
+    if word in strList:
         answer += 1
 
 print(answer)
