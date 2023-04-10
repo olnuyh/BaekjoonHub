6d220863649b57b334e008cc68e185bec9ccbb26
rihyun1126 <49039762+rihyun1126@users.noreply.github.com>
2023-04-10 16:55:01 +0900

    [Silver II] Title: 이항 계수 2, Time: 232 ms, Memory: 87456 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.py
@@ -0,0 +1,15 @@
+import sys
+read = sys.stdin.readline
+
+n, k = map(int, read().split())
+D = [[0] * (n + 1) for _ in range(n + 1)]
+for i in range(1, n + 1):
+    D[i][1] = i
+    D[i][0] = 1
+    D[i][i] = 1
+
+for i in range(3, n + 1):
+    for j in range(2, i):
+        D[i][j] = D[i - 1][j] + D[i - 1][j - 1]
+
+print(D[n][k] % 10007)
