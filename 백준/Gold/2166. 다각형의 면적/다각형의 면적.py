2d0333dc24100aba706c13c35781541d7997068f
rihyun1126 <49039762+rihyun1126@users.noreply.github.com>
2023-04-17 02:45:13 +0900

    [Gold V] Title: 다각형의 면적, Time: 444 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/2166. 다각형의 면적/다각형의 면적.py
+++ 백준/Gold/2166. 다각형의 면적/다각형의 면적.py
@@ -1,16 +1,20 @@
 N = int(input())
-Point = []
-Point.append([])
+X = []
+Y = []
 
 for _ in range(N):
-    x, y = map(int, input().split())
-    Point.append((x, y))
+    x1, y1 = map(int, input().split())
+    X.append(x1)
+    Y.append(y1)
 
-def CCW(x1, y1, x2, y2, x3, y3):
-    return ((x1 * y2 + x2 * y3 + x3 * y1) - (x2 * y1 + x3 * y2 + x1 * y3)) / 2
+X.append(X[0])
+Y.append(Y[0])
+
+def CCW(x1, y1, x2, y2):
+    return x1 * y2 - x2 * y1
 
 answer = 0
-for i in range(2, N):
-    answer += CCW(Point[1][0], Point[1][1], Point[i][0], Point[i][1], Point[i + 1][0], Point[i + 1][1])
+for i in range(N):
+    answer += (X[i] * Y[i + 1] - X[i + 1] * Y[i])
 
-print(abs(answer))
+print(round(abs(answer / 2), 1))
