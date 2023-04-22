f3621e9b00c0a7f85d156621f545b6d962b58555
rihyun1126 <49039762+rihyun1126@users.noreply.github.com>
2023-04-22 16:43:27 +0900

    [Silver III] Title: N과 M (2), Time: 112 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,13 @@
+n, m = map(int, input().split())
+l = []
+def dfs():
+    if len(l) == m:
+        if l == sorted(l):
+            print(' '.join(map(str, l)))
+        return
+    for i in range(1, n + 1):
+        if i not in l:
+            l.append(i)
+            dfs()
+            l.pop()
+dfs()
