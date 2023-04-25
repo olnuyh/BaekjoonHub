3b29c3e2856d04245ea4399b1cf4b8864e514bab
rihyun1126 <49039762+rihyun1126@users.noreply.github.com>
2023-04-25 13:45:08 +0900

    [Silver I] Title: 스타트링크, Time: 564 ms, Memory: 70912 KB -BaekjoonHub

--- 백준/Silver/5014. 스타트링크/스타트링크.py
+++ 백준/Silver/5014. 스타트링크/스타트링크.py
@@ -2,22 +2,20 @@ from collections import deque
 
 F, S, G, U, D = map(int, input().split())
 Building = [0] * (F + 1)
-q = deque()
-q.append(S)
-while q:
-    now = q.popleft()
-    for i in [U, -D]:
-        if i == 0:
-            continue
-        next = now + i
-        if 1 <= next <= F and Building[next] == 0:
-            Building[next] = Building[now] + 1
-            q.append(next)
 
-if S == G:
-    print(0)
-else:
-    if Building[G] == 0:
-        print('use the stairs')
-    else:
-        print(Building[G])
+def BFS():
+    q = deque()
+    q.append(S)
+    Building[S] = 1
+    while q:
+        now = q.popleft()
+        if now == G:
+            return Building[now] - 1
+        for i in [U, -D]:
+            next = now + i
+            if 1 <= next <= F and Building[next] == 0:
+                Building[next] = Building[now] + 1
+                q.append(next)
+    return 'use the stairs'
+
+print(BFS())
