d8e855746d9e4cc293b8c3041d33cabbe72e1992
rihyun1126 <49039762+rihyun1126@users.noreply.github.com>
2023-04-29 23:17:21 +0900

    [Silver II] Title: 스택 수열, Time: 292 ms, Memory: 27484 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int n = Integer.parseInt(br.readLine());
+	
+		int num = 1;
+		Stack<Integer> stack = new Stack();
+		boolean isOk = true;
+		
+		for(int i = 0; i < n; i++) {
+			int find = Integer.parseInt(br.readLine());
+			while(num <= find)
+			{
+				sb.append("+\n");
+				stack.push(num);
+				num++;
+			}
+			if(stack.pop() == find)
+				sb.append("-\n");
+			else {
+				isOk = false;
+			}
+		}
+		
+		if(isOk) {
+			System.out.println(sb.toString());
+		}
+		else
+			System.out.println("NO");
+	}
+}
