70926176fea6a9ed43c968d1ad2d4b0cde374dd5
olnuyh <49039762+olnuyh@users.noreply.github.com>
2023-07-31 00:35:01 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 116 ms, Memory: 13152 KB -BaekjoonHub

--- 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.java
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.java
@@ -4,26 +4,29 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
+		StringBuilder sb = new StringBuilder();
+		
 		int n = sc.nextInt();
 		
 		for(int i = 1; i <= n; i++) {
 			for(int j = 1; j <= n - i; j++)
-				System.out.print(" ");
+				sb.append(" ");
 			
 			for(int j = 1; j <= i; j++)
-				System.out.print("*");
-			System.out.println();
+				sb.append("*");
+			sb.append("\n");
 		}
 		
 		for(int i = n - 1; i >= 1; i--) {
 			for(int j = 1; j <= n - i; j++)
-				System.out.print(" ");
+				sb.append(" ");
 			
 			for(int j = 1; j <= i; j++)
-				System.out.print("*");
-			System.out.println();
+				sb.append("*");
+			sb.append("\n");
 		}
+		
+		System.out.println(sb);
 	}
 
 }
