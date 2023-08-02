54ae5007297a73d4bc1355ca481cc626f0a773da
olnuyh <49039762+olnuyh@users.noreply.github.com>
2023-08-02 16:14:36 +0900

    [D4] Title: 햄스터, Time: 1,526 ms, Memory: 106,392 KB -BaekjoonHub

---
+++ SWEA/D4/8275. 햄스터/README.md
@@ -0,0 +1,11 @@
+# [D4] 햄스터 - 8275 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWxQ310aOlQDFAWL) 
+
+### 성능 요약
+
+메모리: 106,392 KB, 시간: 1,526 ms, 코드길이: 1,925 Bytes
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/8275. 햄스터/햄스터.java
@@ -0,0 +1,84 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Solution {
+	public static StringBuilder sb = new StringBuilder();
+	public static int n, x, m;
+	public static int[][] question;
+	
+	public static int max;
+	public static int[] answer;
+	
+	public static void permutation(int r, int[] output) {
+		if(r == n) {
+			int[] sum = new int[n + 1];
+			
+			for(int i = 1; i <= n; i++)
+				sum[i] = sum[i - 1] + output[i - 1];
+			
+			for(int i = 0; i < question.length; i++) {
+				if(question[i][2] != (sum[question[i][1]] - sum[question[i][0] - 1]))
+					return;
+			}
+			
+			if(max < sum[n]) {
+				max = sum[n];
+				
+				for(int i = 0; i < n; i++)
+					answer[i] = output[i];
+			}
+			
+			return;
+		}
+		
+		for(int i = 0; i <= x; i++) {
+			output[r] = i;
+			permutation(r + 1, output);
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int t = Integer.parseInt(br.readLine());
+		
+		for(int tc = 1; tc <= t; tc++) {
+			st = new StringTokenizer(br.readLine());
+			
+			n = Integer.parseInt(st.nextToken());
+			x = Integer.parseInt(st.nextToken());
+			m = Integer.parseInt(st.nextToken());
+			
+			question = new int[m][3];
+			
+			for(int i = 0; i < m; i++) {
+				st = new StringTokenizer(br.readLine());
+				question[i][0] = Integer.parseInt(st.nextToken());
+				question[i][1] = Integer.parseInt(st.nextToken());
+				question[i][2] = Integer.parseInt(st.nextToken());
+			}
+			
+			max = Integer.MIN_VALUE;
+			answer = new int[n];
+			
+			permutation(0, new int[n]);
+			
+			sb.append("#").append(tc).append(" ");
+			
+			if(max == Integer.MIN_VALUE)
+				sb.append("-1\n");
+			else {
+				for(int i = 0; i < n; i++)
+					sb.append(answer[i] + " ");
+				sb.append("\n");
+			}
+		}
+		
+		System.out.println(sb);
+	}
+
+}
