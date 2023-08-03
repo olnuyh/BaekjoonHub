a64c49a07e27950c9fa1bdf34e8bf94c02eed0c3
olnuyh <49039762+olnuyh@users.noreply.github.com>
2023-08-03 16:29:01 +0900

    [Gold V] Title: 신기한 소수, Time: 76 ms, Memory: 11400 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -1,36 +1,43 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-public class Main{
+public class Main {
 	static int n;
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		n = sc.nextInt();
-		
-		DFS(2, 1);
-		DFS(3, 1);
-		DFS(5, 1);
-		DFS(7, 1);
-	}
+	static StringBuilder sb;
 	
-	static void DFS(int num, int jarisu) {
-		if(jarisu == n) {
-			System.out.println(num);
-			return;
-		}
-		
-		for(int i = 1; i < 10; i++) {
-			if(i % 2 == 0)
-				continue;
-			if(isPrime(num * 10 + i))
-				DFS(num * 10 + i, jarisu + 1);
-		}
-	}
-	
-	static boolean isPrime(int num) {
+	public static boolean isDecimal(int num) {
 		for(int i = 2; i <= Math.sqrt(num); i++) {
 			if(num % i == 0)
 				return false;
 		}
 		return true;
 	}
+	
+	public static void func(int r, int num) {
+		if(r == n) {
+			sb.append(num).append("\n");
+			return;
+		}
+		
+		for(int i = 1; i < 10; i += 2) {
+			if(isDecimal(num * 10 + i))
+				func(r + 1, num * 10 + i);
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
+
+		n = Integer.parseInt(br.readLine());
+		
+		int[] nums = {2, 3, 5, 7};
+		
+		for(int num : nums)
+			func(1, num);
+		
+		System.out.println(sb);
+	}
+
 }
