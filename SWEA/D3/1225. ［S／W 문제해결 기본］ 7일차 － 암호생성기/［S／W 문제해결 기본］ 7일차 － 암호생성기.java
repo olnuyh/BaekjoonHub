66184a22d048761bd4722254e6d4ec9033a09523
olnuyh <49039762+olnuyh@users.noreply.github.com>
2023-08-06 23:21:29 +0900

    [D3] Title: [S/W 문제해결 기본] 7일차 - 암호생성기, Time: 118 ms, Memory: 20,976 KB -BaekjoonHub

--- SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
+++ SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
@@ -1,42 +1,52 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.Queue;
-import java.util.LinkedList;
+import java.util.StringTokenizer;
 
-class Solution
-{
-	public static void main(String args[]) throws Exception
-	{
-		Scanner sc = new Scanner(System.in);
-        while(sc.hasNext())
-        {
-        	int T = sc.nextInt();
-            Queue<Integer> q = new LinkedList<>();
+public class Solution {
 
-            for(int i = 0; i < 8; i++)
-                q.add(sc.nextInt());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		
+		for(int t = 1; t <= 10; t++) {
+			int tc = Integer.parseInt(br.readLine());
+			
+			Queue<Integer> queue = new ArrayDeque<>();
+			st = new StringTokenizer(br.readLine());
+			
+			for(int i = 0; i < 8; i++)
+				queue.offer(Integer.parseInt(st.nextToken()));
+			
+			int d = 1;
+			
+			while(true) {
+				int temp = queue.poll();
+				
+				if(temp - d <= 0) {
+					queue.offer(0);
+					break;
+				}
+				
+				queue.offer(temp - d);
+				
+				d = (d + 1) % 6;
+				
+				if(d == 0)
+					d = 1;
+			}
+			
+			sb.append("#").append(tc).append(" ");
+			
+			for(int i = 0; i < 8; i++)
+				sb.append(queue.poll()).append(" ");
+			sb.append("\n");
+		}
 
-            int[] num = {1, 2, 3, 4, 5};
-            int index = 0;
-
-            while(true)
-            {
-                int a = q.poll();
-                if(a - num[index] <= 0)
-                {
-                    q.add(0);
-                    break;
-                }
-                else
-                {
-                    q.add(a - num[index]);
-                    index = (index + 1) % 5;
-                }
-        	}
-            
-            System.out.printf("#%d ", T);
-        	for(int i = 0; i < 8; i++)
-            	System.out.print(q.poll() + " ");
-        	System.out.println();
-        }
+		System.out.println(sb);
 	}
+
 }
