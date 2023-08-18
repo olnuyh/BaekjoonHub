188b3277df7984af4a43e95f6180eecc1cad86c0
olnuyh <49039762+olnuyh@users.noreply.github.com>
2023-08-18 12:08:15 +0900

    [Silver I] Title: 미로 탐색, Time: 92 ms, Memory: 12052 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,55 +1,56 @@
-import java.util.LinkedList;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
-public class Main{
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int m = sc.nextInt();
+public class Main {
+	public static int n, m;
+	public static int[][] maze;
+	public static int[][] cnt;
+	public static int[] dr = {-1, 1, 0, 0};
+	public static int[] dc = {0, 0, -1, 1};
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int [][] A = new int[n][m];
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		maze = new int[n][m];
+		cnt = new int[n][m];
+		
 		for(int i = 0; i < n; i++) {
-			char[] tmp = sc.next().toCharArray();
+			String str = br.readLine();
 			for(int j = 0; j < m; j++)
-				A[i][j] = tmp[j] - '0';
+				maze[i][j] = str.charAt(j) - '0';
 		}
 		
-		boolean[][] visited = new boolean[n][m];
-		int[] dx = {1, -1, 0, 0};
-		int[] dy = {0, 0, 1, -1};
+		bfs(0, 0);
+		System.out.println(cnt[n - 1][m - 1]);
+	}
+	
+	public static void bfs(int sr, int sc) {
+		Queue<int []> queue = new ArrayDeque<>();
+		queue.offer(new int[] {sr, sc});
+		cnt[sr][sc] = 1;
 		
-		Queue<Node> q = new LinkedList<>();
-		q.add(new Node(0, 0));
-		visited[0][0] = true;
-		
-		while(!q.isEmpty()) {
-			Node now = q.poll();
-			
+		while(!queue.isEmpty()) {
+			int[] now = queue.poll();
 			for(int i = 0; i < 4; i++) {
-				int next_r = now.r + dx[i];
-				int next_c = now.c + dy[i];
+				int nr = now[0] + dr[i];
+				int nc = now[1] + dc[i];
 				
-				if(next_r >= 0 && next_r < n && next_c >= 0 && next_c < m) {
-					if(A[next_r][next_c] == 1 && !visited[next_r][next_c]) {
-						visited[next_r][next_c] = true;
-						q.add(new Node(next_r, next_c));
-						A[next_r][next_c] = A[now.r][now.c] + 1;
-					}
+				if(nr < 0 || nr >= n || nc < 0 || nc >= m) continue;
+				
+				if(cnt[nr][nc] == 0 && maze[nr][nc] == 1) {
+					queue.offer(new int[] {nr, nc});
+					cnt[nr][nc] = cnt[now[0]][now[1]] + 1;
 				}
 			}
 		}
-		System.out.println(A[n - 1][m - 1]);
 	}
-}
 
-class Node{
-	int r;
-	int c;
-	
-	Node(int r, int c)
-	{
-		this.r = r;
-		this.c = c;
-	}
 }
