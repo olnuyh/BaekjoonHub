b63076b297b755a100198ba4ba90ed65ba1b59d8
olnuyh <49039762+olnuyh@users.noreply.github.com>
2023-08-18 11:45:17 +0900

    [Silver II] Title: DFS와 BFS, Time: 184 ms, Memory: 17944 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,72 +1,79 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
-public class Main{
-	static ArrayList<Integer>[] A;
-	static boolean[] visited;
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-		
-		int n = sc.nextInt();
-		int m = sc.nextInt();
-		int v = sc.nextInt();
-		
-		A = new ArrayList[n + 1];
-		
-		for(int i = 1; i <= n; i++)
-			A[i] = new ArrayList<Integer>();
-		
-		for(int i = 0; i < m; i++) {
-			int s = sc.nextInt();
-			int e = sc.nextInt();
-			
-			A[s].add(e);
-			A[e].add(s);
-		}
-		
-		for(int i = 1; i <= n; i++)
-			Collections.sort(A[i]);
-		
-		visited = new boolean[n + 1];
-		DFS(v);
-		System.out.println();
-		
-		visited = new boolean[n + 1];
-		BFS(v);
-	}
+public class Main {
+	public static boolean[] visited;
+	public static StringBuilder sb;
+	public static ArrayList<Integer>[] list;
 	
-	public static void DFS(int num){
-		if(visited[num])
-			return;
-		
+	public static void dfs(int num) {
 		visited[num] = true;
-		System.out.print(num + " ");
+		sb.append(num + " ");
 		
-		for(int i : A[num]) {
-			if(!visited[i])
-				DFS(i);
+		for(int i = 0; i < list[num].size(); i++) {
+			int next = list[num].get(i);
+			if(!visited[next])
+				dfs(next);
 		}
 	}
 	
-	public static void BFS(int num){
-		Queue<Integer> q = new LinkedList<>();
+	public static void bfs(int num) {
+		Queue<Integer> q = new ArrayDeque<>();
 		q.add(num);
 		visited[num] = true;
 		
 		while(!q.isEmpty()) {
-			int now = q.poll();
-			System.out.print(now + " ");
-			
-			for(int i : A[now]) {
-				if(!visited[i]) {
-					visited[i] = true;
-					q.add(i);
+			int v = q.poll();
+			sb.append(v + " ");
+			for(int i = 0; i < list[v].size(); i++) {
+				int next = list[v].get(i);
+				if(!visited[next]) {
+					q.add(next);
+					visited[next] = true;
 				}
 			}
 		}
 	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		sb = new StringBuilder();
+		
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int v = Integer.parseInt(st.nextToken());
+		
+		list = new ArrayList[n + 1];
+		
+		for(int i = 1; i <= n; i++)
+			list[i] = new ArrayList<>();
+		
+		for(int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int s = Integer.parseInt(st.nextToken());
+			int e = Integer.parseInt(st.nextToken());
+			
+			list[s].add(e);
+			list[e].add(s);
+		}
+		
+		for(int i = 1; i <= n; i++)
+			Collections.sort(list[i]);
+		
+		visited = new boolean[n + 1];
+		dfs(v);
+		sb.append("\n");
+		visited = new boolean[n + 1];
+		bfs(v);
+		
+		System.out.println(sb);
+	}
+
 }
