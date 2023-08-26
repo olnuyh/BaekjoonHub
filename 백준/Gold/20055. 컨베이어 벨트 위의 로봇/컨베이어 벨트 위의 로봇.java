603496dbdde9fb3635bd4c3d8d0350d9530ce984
olnuyh <49039762+olnuyh@users.noreply.github.com>
2023-08-27 01:59:33 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 236 ms, Memory: 14452 KB -BaekjoonHub

--- 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.java
+++ 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.java
@@ -1,9 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -22,49 +19,32 @@ public class Main {
 			durability[i] = Integer.parseInt(st.nextToken());
 		
 		int stage = 1;
-		Queue<Integer> q = new ArrayDeque<>();
-		boolean[] robot = new boolean[2 * n];
+		boolean[] robot = new boolean[n];
 		
 		while(true) {
 			// 1. 벨트 + 로봇 회전
 			int tempBelt = durability[2 * n - 1];
-			boolean tempRobot = robot[2 * n - 1];
 			
-			for(int i = 2 * n - 1; i > 0; i--) {
+			for(int i = 2 * n - 1; i > 0; i--)
 				durability[i] = durability[i - 1];
-				robot[i] = robot[i - 1];
-			}
-			
 			durability[0] = tempBelt;
-			robot[0] = tempRobot;
+			
+			for(int i = n - 1; i > 0; i--)
+				robot[i] = robot[i - 1];
+			robot[0] = false;
+			robot[n - 1] = false; // 벨트 회전 시 내리는 위치에 오면 로봇 제거
 			
 			// 2. 로봇 이동
-			if(!q.isEmpty()) {
-				int size = q.size();
-				
-				for(int i = 0; i < size; i++) {
-					int pos = q.poll();
-					
-					if(pos + 1 == n - 1) { // 벨트가 회전했을 때 내리는 위치에 오면 삭제
-						robot[pos + 1] = false;
-						continue;
-					}
-					
-					if(!robot[pos + 2] && durability[pos + 2] >= 1) { // 로봇 이동이 가능할 때
-						durability[pos + 2] -= 1;
-						robot[pos + 1] = false;
-						if(pos + 2 != n - 1) { // 내리는 위치에 도달하지 않을 경우 다시 로봇 큐에 추가
-							q.add(pos + 2);
-							robot[pos + 2] = true;
-						}
-					}else // 로봇 이동이 불가능할 때
-						q.add(pos + 1);
+			for(int i = n - 1; i > 0; i--) {
+				if(robot[i - 1] && !robot[i] && durability[i] >= 1) {
+					robot[i] = true;
+					robot[i - 1] = false;
+					durability[i] -= 1;
 				}
 			}
 			
-			// 3. 로봇 올리기
+			// 3. 올리는 위치에 로봇 올리기
 			if(durability[0] > 0) {
-				q.add(0);
 				durability[0] -= 1;
 				robot[0] = true;
 			}
