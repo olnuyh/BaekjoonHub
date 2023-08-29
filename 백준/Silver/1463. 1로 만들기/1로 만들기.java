f65b464116cac1e6dd5aed639b6ca6b5478a3371
olnuyh <49039762+olnuyh@users.noreply.github.com>
2023-08-29 12:23:27 +0900

    [Silver III] Title: 1로 만들기, Time: 128 ms, Memory: 15880 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,21 +1,30 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
-public class Main{
-	public static void main(String[] args) throws Exception{
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int[] D = new int[N + 1];
-		D[1] = 0;
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
 		
-		for(int i = 2; i <= N; i++)
-		{
-			D[i] = D[i - 1] + 1;
-			if(i % 2 == 0)
-				D[i] = Math.min(D[i], D[i / 2] + 1);
-			if(i % 3 == 0)
-				D[i] = Math.min(D[i], D[i / 3] + 1);
+		int[] D = new int[n + 1];
+		Arrays.fill(D, Integer.MAX_VALUE);
+		D[n] = 0;
+		
+		for(int i = n - 1; i >= 1; i--) {
+			if(i * 3 <= n)
+				D[i] = Math.min(D[i], D[i * 3] + 1);
+			
+			if(i * 2 <= n)
+				D[i] = Math.min(D[i], D[i * 2] + 1);
+			
+			D[i] = Math.min(D[i], D[i + 1] + 1);
 		}
 		
-		System.out.println(D[N]);
+		System.out.println(D[1]);
 	}
+
 }
