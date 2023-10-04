3ae69dc9a2f1f8dbcc4f55adbd8ea06acbfa5437
olnuyh <49039762+olnuyh@users.noreply.github.com>
2023-10-04 12:43:59 +0900

    [Platinum V] Title: 요세푸스 문제 3, Time: 124 ms, Memory: 11908 KB -BaekjoonHub

---
+++ 백준/Platinum/11025. 요세푸스 문제 3/요세푸스 문제 3.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		int dp=0;
+		for (int i = 1; i <= n; i++) {
+			dp = (dp+k)%i;
+		}
+		System.out.println(dp+1);
+	}
+}
