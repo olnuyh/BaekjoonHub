84a0f494983902bb68ef2259ebc3afe61dfa2c2f
olnuyh <49039762+olnuyh@users.noreply.github.com>
2024-04-28 12:01:00 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -0,0 +1,2 @@
+-- 코드를 입력하세요
+SELECT concat('/home/grep/src/', b.board_id, '/', f.file_id, f.file_name, f.file_ext) as FILE_PATH from used_goods_board b join used_goods_file f on b.board_id = f.board_id where b.views = (select max(views) from used_goods_board) order by f.file_id desc;
