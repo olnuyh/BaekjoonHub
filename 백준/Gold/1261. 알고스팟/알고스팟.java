6ee4354d54acee2c7416c387cac6f1e2d2f42aff
olnuyh <49039762+olnuyh@users.noreply.github.com>
2024-06-16 18:10:31 +0900

    [Gold IV] Title: 알고스팟, Time: 84 ms, Memory: 12080 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.java
+++ 백준/Gold/1261. 알고스팟/알고스팟.java
@@ -1,25 +1,27 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-class Room implements Comparable<Room>{
-    int r, c, wall;
-
-    public Room(int r, int c, int wall) {
-        this.r = r;
-        this.c = c;
-        this.wall = wall;
-    }
-
-    @Override
-    public int compareTo(Room o) {
-        return this.wall - o.wall;
-    }
-}
-
 public class Main {
+    static class Room implements Comparable<Room>{
+        int r, c, wall;
+
+        public Room(int r, int c, int wall) {
+            this.r = r;
+            this.c = c;
+            this.wall = wall;
+        }
+
+        @Override
+        public int compareTo(Room o) {
+            return this.wall - o.wall;
+        }
+    }
+
     public static int[][] deltas = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
     public static int M, N;
     public static int[][] maze;
@@ -55,24 +57,29 @@ public class Main {
     }
 
     public static void move() {
-        PriorityQueue<Room> pq = new PriorityQueue<>();
+        Deque<int[]> dq = new ArrayDeque<>();
         result[0][0] = 0;
-        pq.add(new Room(0, 0, 0));
+        dq.add(new int[]{0, 0});
 
-        while (!pq.isEmpty()) {
-            Room room = pq.poll();
+        while (!dq.isEmpty()) {
+            int[] cur = dq.pollFirst();
 
             for (int i = 0; i < 4; i++) {
-                int nr = room.r + deltas[i][0];
-                int nc = room.c + deltas[i][1];
+                int nr = cur[0] + deltas[i][0];
+                int nc = cur[1] + deltas[i][1];
 
                 if (nr < 0 || nr >= N || nc < 0 || nc >= M || result[nr][nc] != Integer.MAX_VALUE) {
                     continue;
                 }
 
-                if (result[nr][nc] > maze[nr][nc] + room.wall) {
-                    result[nr][nc] = maze[nr][nc] + room.wall;
-                    pq.add(new Room(nr, nc, result[nr][nc]));
+                if (result[nr][nc] > result[cur[0]][cur[1]] + maze[nr][nc]) {
+                    result[nr][nc] = result[cur[0]][cur[1]] + maze[nr][nc];
+
+                    if (maze[nr][nc] == 0) {
+                        dq.addFirst(new int[]{nr, nc});
+                    } else {
+                        dq.addLast(new int[]{nr, nc});
+                    }
                 }
             }
         }
