58d59d7aec7e0a33942858cb6fc2c0c8ff09f4db
olnuyh <49039762+olnuyh@users.noreply.github.com>
2024-06-16 17:54:07 +0900

    [Gold IV] Title: 알고스팟, Time: 100 ms, Memory: 12400 KB -BaekjoonHub

---
+++ 백준/Gold/1261. 알고스팟/알고스팟.java
@@ -0,0 +1,80 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+class Room implements Comparable<Room>{
+    int r, c, wall;
+
+    public Room(int r, int c, int wall) {
+        this.r = r;
+        this.c = c;
+        this.wall = wall;
+    }
+
+    @Override
+    public int compareTo(Room o) {
+        return this.wall - o.wall;
+    }
+}
+
+public class Main {
+    public static int[][] deltas = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    public static int M, N;
+    public static int[][] maze;
+    public static int[][] result;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+
+        maze = new int[N][M];
+        result = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            char[] tmp = br.readLine().toCharArray();
+
+            for (int j = 0; j < M; j++) {
+                maze[i][j] = tmp[j] - '0';
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                result[i][j] = Integer.MAX_VALUE;
+            }
+        }
+
+        move();
+
+        System.out.println(result[N - 1][M - 1]);
+    }
+
+    public static void move() {
+        PriorityQueue<Room> pq = new PriorityQueue<>();
+        result[0][0] = 0;
+        pq.add(new Room(0, 0, 0));
+
+        while (!pq.isEmpty()) {
+            Room room = pq.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nr = room.r + deltas[i][0];
+                int nc = room.c + deltas[i][1];
+
+                if (nr < 0 || nr >= N || nc < 0 || nc >= M || result[nr][nc] != Integer.MAX_VALUE) {
+                    continue;
+                }
+
+                if (result[nr][nc] > maze[nr][nc] + room.wall) {
+                    result[nr][nc] = maze[nr][nc] + room.wall;
+                    pq.add(new Room(nr, nc, result[nr][nc]));
+                }
+            }
+        }
+    }
+}
