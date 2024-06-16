9769aa2f3abbd0fb395c3d5cdee861cff23f7a29
olnuyh <49039762+olnuyh@users.noreply.github.com>
2024-06-16 16:35:41 +0900

    [Gold V] Title: 캠프 준비, Time: 104 ms, Memory: 12048 KB -BaekjoonHub

--- 백준/Gold/16938. 캠프 준비/캠프 준비.java
+++ 백준/Gold/16938. 캠프 준비/캠프 준비.java
@@ -4,21 +4,17 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static int N, L, R, X;
-    public static int[] A;
-    public static int count;
-
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        L = Integer.parseInt(st.nextToken());
-        R = Integer.parseInt(st.nextToken());
-        X = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
 
-        A = new int[N];
+        int[] A = new int[N];
 
         st = new StringTokenizer(br.readLine());
 
@@ -26,44 +22,32 @@ public class Main {
             A[i] = Integer.parseInt(st.nextToken());
         }
 
-        count = 0;
+        int count = 0;
 
-        chooseProblem(0, 0, 0, new boolean[N]);
-
-        System.out.println(count);
-    }
-
-    public static void chooseProblem(int totalNum, int selectedNum, int start, boolean[] selected) {
-        if (totalNum == N) {
-
-            if (selectedNum < 2) {
-                return;
-            }
-
+        for (int i = 1; i < (1 << N); i++) {
+            int selected = 0;
             int sum = 0;
             int minLevel = Integer.MAX_VALUE;
-            int maxLevel = 0;
+            int maxLevel = Integer.MIN_VALUE;
 
-            for (int i = 0; i < N; i++) {
-                if (selected[i]) {
-                    sum += A[i];
-                    minLevel = Integer.min(minLevel, A[i]);
-                    maxLevel = Integer.max(maxLevel, A[i]);
+            for (int j = 0; j < N; j++) {
+                if ((i & (1 << j)) != 0) {
+                    selected++;
+                    sum += A[j];
+                    minLevel = Integer.min(minLevel, A[j]);
+                    maxLevel = Integer.max(maxLevel, A[j]);
                 }
+            }
+
+            if (selected < 2) {
+                continue;
             }
 
             if (sum >= L && sum <= R && maxLevel - minLevel >= X) {
                 count++;
             }
-
-            return;
         }
 
-        for (int i = start; i < N; i++) {
-            selected[i] = false;
-            chooseProblem(totalNum + 1, selectedNum, i + 1, selected);
-            selected[i] = true;
-            chooseProblem(totalNum + 1, selectedNum + 1, i + 1, selected);
-        }
+        System.out.println(count);
     }
 }
