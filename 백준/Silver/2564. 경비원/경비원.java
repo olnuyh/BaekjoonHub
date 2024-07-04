17a2583543fe26b710e96ff44c7776d21000a5b8
olnuyh <49039762+olnuyh@users.noreply.github.com>
2024-07-04 15:08:48 +0900

    [Silver I] Title: 경비원, Time: 80 ms, Memory: 11560 KB -BaekjoonHub

--- 백준/Silver/2564. 경비원/경비원.java
+++ 백준/Silver/2564. 경비원/경비원.java
@@ -4,8 +4,6 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static int[] opposite = {0, 2, 1, 4, 3};
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -15,45 +13,36 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
 
-        int[][] stores = new int[N + 1][3];
+        int[] stores = new int[N + 1];
 
         for (int i = 0; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
 
-            stores[i][0] = Integer.parseInt(st.nextToken());
+            int dir = Integer.parseInt(st.nextToken());
             int num = Integer.parseInt(st.nextToken());
 
-            switch (stores[i][0]) {
+            switch (dir) {
                 case 1:
-                    stores[i][1] = 0;
-                    stores[i][2] = num;
+                    stores[i] = num;
                     break;
                 case 2:
-                    stores[i][1] = height;
-                    stores[i][2] = num;
+                    stores[i] = width + height + (width - num);
                     break;
                 case 3:
-                    stores[i][1] = num;
-                    stores[i][2] = 0;
+                    stores[i] = 2 * (width + height) - num;
                     break;
                 case 4:
-                    stores[i][1] = num;
-                    stores[i][2] = width;
+                    stores[i] = width + num;
                     break;
             }
         }
 
         int sum = 0;
+        int total = 2 * (width + height);
 
         for (int i = 0; i < N; i++) {
-            int dist = Math.abs(stores[N][1] - stores[i][1]) + Math.abs(stores[N][2] - stores[i][2]);
-
-            if (opposite[stores[N][0]] == stores[i][0]) {
-                int dist2 = 2 * (Math.max(stores[N][1], stores[i][1]) + Math.max(stores[N][2], stores[i][2])) - dist;
-                sum += Math.min(dist2, 2 * (width + height) - dist2);
-            } else {
-                sum += Math.min(dist, 2 * (width + height) - dist);
-            }
+            int dist = Math.abs(stores[i] - stores[N]);
+            sum += Math.min(dist, total - dist);
         }
 
         System.out.println(sum);
