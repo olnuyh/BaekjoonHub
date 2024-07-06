7c8bf90770afdf33da91725213dc14390644d8b6
olnuyh <49039762+olnuyh@users.noreply.github.com>
2024-07-06 11:49:57 +0900

    [Silver III] Title: N과 M (2), Time: 80 ms, Memory: 11548 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -4,32 +4,42 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static int n, m;
-	public static StringBuilder sb = new StringBuilder();
-	
-	public static void comb(int nthChoice, int startIndex, int[] choosed) {
-		if(nthChoice == m) {
-			for(int i = 0; i < m; i++)
-				sb.append(choosed[i] + " ");
-			sb.append("\n");
-			return;
-		}
-			
-		for(int i = startIndex; i <= n; i++) {
-			choosed[nthChoice] = i;
-			comb(nthChoice + 1, i + 1, choosed);
-		}
-	}
+    public static int[] nums;
+    public static int N,M;
+    public static StringBuilder sb;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		
-		comb(0, 1, new int[m]);
-		System.out.println(sb);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
 
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        nums = new int[N];
+        for (int i = 0; i < N; i++) {
+            nums[i] = i + 1;
+        }
+
+        makeNums(0, 0, new int[M]);
+
+        System.out.println(sb);
+    }
+
+    public static void makeNums(int count, int cur, int[] selected) {
+        if (count == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(selected[i]).append(" ");
+            }
+
+            sb.append("\n");
+
+            return;
+        }
+
+        for (int i = cur; i < N; i++) {
+            selected[count] = nums[i];
+            makeNums(count + 1, i + 1, selected);
+        }
+    }
 }
