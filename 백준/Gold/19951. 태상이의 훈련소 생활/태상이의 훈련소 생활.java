b18229e89089d5a55fceab5530d35b9e388b9569
olnuyh <49039762+olnuyh@users.noreply.github.com>
2024-07-10 14:36:35 +0900

    [Gold V] Title: 태상이의 훈련소 생활, Time: 484 ms, Memory: 63880 KB -BaekjoonHub

--- 백준/Gold/19951. 태상이의 훈련소 생활/태상이의 훈련소 생활.java
+++ 백준/Gold/19951. 태상이의 훈련소 생활/태상이의 훈련소 생활.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
@@ -37,7 +38,9 @@ public class Main {
         }
 
         for (int i = 0; i < N; i++) {
-            System.out.print(original[i] + variation[i] + " ");
+            sb.append(original[i] + variation[i]).append(" ");
         }
+
+        System.out.println(sb);
     }
 }
