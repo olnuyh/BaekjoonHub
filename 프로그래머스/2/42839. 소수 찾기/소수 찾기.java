5acdec360f56aa012dfee13e5f1ed9220d930fc0
olnuyh <49039762+olnuyh@users.noreply.github.com>
2024-07-31 21:44:21 +0900

    [level 2] Title: 소수 찾기, Time: 25.03 ms, Memory: 70.9 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,51 +1,44 @@
 import java.util.*;
+import java.util.stream.*;
 
 class Solution {
-    public static ArrayList<Integer> list;
-    public static char[] nums;
-    public static boolean[] selected;
-    
-    public int solution(String numbers) {
-        nums = numbers.toCharArray();
-        list = new ArrayList();
+    public int solution(String numbers) {     
+        Set<Integer> primes = new HashSet();
+
+        int[] nums = numbers.chars()
+            .map(c -> c - '0')
+            .toArray();
         
-        selected = new boolean[numbers.length()];
-        makeNumber("", 0);
+        getPrimes(0, nums, new boolean[nums.length], primes);
         
-        return list.size();
+        return primes.size();
     }
     
-    public void makeNumber(String cur, int idx) {
-        if (idx == selected.length) {
-            if (cur.equals("")) {
-                return;
-            }
-            
-            int num = Integer.parseInt(cur);
-            
-            if (check(num) && !list.contains(num)) {
-                list.add(num);
-            }
-            return;
+    private void getPrimes(int acc, int[] numbers, boolean[] isUsed, Set<Integer> primes) {
+        if (isPrime(acc)) {
+            primes.add(acc);
         }
         
-        for (int i = 0; i < nums.length; i++) {
-            if (!selected[i]) {
-                selected[i] = true;
-                makeNumber(cur + nums[i], idx + 1);
-                selected[i] = false;
-                makeNumber(cur, idx + 1);     
-            } 
+        for (int i = 0; i < numbers.length; i++) {
+            if (isUsed[i]) {
+                continue;
+            }
+            
+            int nextAcc = acc * 10 + numbers[i];
+            
+            isUsed[i] = true;
+            getPrimes(nextAcc, numbers, isUsed, primes);
+            isUsed[i] = false;
         }
     }
     
-    public boolean check(int number) {
-        if (number <= 1) {
+    private boolean isPrime(int n) {
+        if (n <= 1) {
             return false;
         }
         
-        for (int i = 2; i <= (int)Math.sqrt(number); i++) {
-            if (number % i == 0) {
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
                 return false;
             }
         }
