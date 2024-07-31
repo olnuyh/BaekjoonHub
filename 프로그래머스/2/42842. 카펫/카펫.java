3c0845421a89de1ac0fb3f48ca3b65e1b7190305
olnuyh <49039762+olnuyh@users.noreply.github.com>
2024-07-31 16:57:37 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 70.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        
+        int total = brown + yellow;
+        for (int i = 3; i <= total; i++) {
+            if (total % i == 0 && (i - 2) * (total / i - 2) == yellow) {
+                answer[0] = total / i;
+                answer[1] = i;
+                
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
