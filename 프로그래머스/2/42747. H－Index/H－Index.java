a81fc614f7984ee4ca76141cc40bd1048388e64a
olnuyh <49039762+olnuyh@users.noreply.github.com>
2024-08-01 16:14:58 +0900

    [level 2] Title: H-Index, Time: 0.83 ms, Memory: 71.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+        
+        Arrays.sort(citations);
+        
+        for (int i = 0; i <= citations.length - 1; i++) {
+            if (citations[i] >= citations.length - i) {
+                answer = citations.length - i;
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
