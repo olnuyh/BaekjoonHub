cedb79003d6bb43e52a2d0b81745357b4147333f
olnuyh <49039762+olnuyh@users.noreply.github.com>
2024-08-22 10:13:14 +0900

    [level 2] Title: 주차 요금 계산, Time: 11.00 ms, Memory: 91.8 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -1,65 +1,76 @@
 import java.util.*;
 
 class Solution {
-    class Car implements Comparable<Car>{
-        int num;
-        int price;
-        
-        public Car (int num, int price) {
-            this.num = num;
-            this.price = price;
-        }
-        
-        public int compareTo(Car c) {
-            return this.num - c.num;
-        }
-    }
-    
     public int[] solution(int[] fees, String[] records) {
-        HashMap<Integer, List<Integer>> hm = new HashMap();
+        Map<String, Integer> map = new TreeMap();
+        
         for (String str : records) {
             String[] record = str.split(" ");
             
-            int num = Integer.parseInt(record[1]);
             String[] time = record[0].split(":");
+            int second = Integer.parseInt(time[0]) * 60 + Integer.parseInt(time[1]);
+            boolean isIn = record[2].equals("IN");
             
-            hm.putIfAbsent(num, new ArrayList());
-            hm.get(num).add(Integer.parseInt(time[0]) * 60 + Integer.parseInt(time[1]));
+            if(isIn) {
+                second *= -1;
+            }
+            
+            map.put(record[1], map.getOrDefault(record[1], 0) + second);
         }
         
-        Car[] cars = new Car[hm.keySet().size()];
+        int[] answer = new int[map.size()];
+        
         int idx = 0;
         
-        for (int num : hm.keySet()) {
-            int sum = 0;
+        for (String num : map.keySet()) {
+            int time = map.get(num);
             
-            List<Integer> list = hm.get(num);
-            
-            for (int i = 0; i < list.size() - 1; i += 2) {
-                sum += list.get(i + 1) - list.get(i);
+            if (time < 1) {
+                time += 23 * 60 + 59;
             }
             
-            if (list.size() % 2 != 0) {
-                sum += 23 * 60 + 59 - list.get(list.size() - 1);
+            int price = fees[1];
+            time -= fees[0];
+            
+            while (time > 0) {
+                price += fees[3];
+                time -= fees[2];
             }
             
-            int price = 0;
-            if (sum <= fees[0]) {
-                price = fees[1];
-            } else {
-                price = fees[1] + (int) Math.ceil((double)(sum - fees[0]) / fees[2]) * fees[3];
-            }
-            
-            cars[idx++] = new Car(num, price);
+            answer[idx++] = price;
         }
         
-        Arrays.sort(cars);
+//         Car[] cars = new Car[hm.keySet().size()];
+//         int idx = 0;
         
-        int[] answer = new int[cars.length];
+//         for (int num : hm.keySet()) {
+//             int sum = 0;
+            
+//             List<Integer> list = hm.get(num);
+            
+//             for (int i = 0; i < list.size() - 1; i += 2) {
+//                 sum += list.get(i + 1) - list.get(i);
+//             }
+            
+//             if (list.size() % 2 != 0) {
+//                 sum += 23 * 60 + 59 - list.get(list.size() - 1);
+//             }
+            
+//             int price = 0;
+//             if (sum <= fees[0]) {
+//                 price = fees[1];
+//             } else {
+//                 price = fees[1] + (int) Math.ceil((double)(sum - fees[0]) / fees[2]) * fees[3];
+//             }
+            
+//             cars[idx++] = new Car(num, price);
+//         }
         
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = cars[i].price;
-        }
+//         Arrays.sort(cars);
+        
+//         for (int i = 0; i < answer.length; i++) {
+//             answer[i] = cars[i].price;
+//         }
         
         return answer;
     }
