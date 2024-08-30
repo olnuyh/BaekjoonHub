5cac9f674fa5f2f7f6c33acbf40d335c043d60f6
olnuyh <49039762+olnuyh@users.noreply.github.com>
2024-08-30 13:33:14 +0900

    [level 3] Title: 야근 지수, Time: 297.94 ms, Memory: 86.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int n, int[] works) {
+        long answer = 0;
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> o2 - o1);
+        for (int work : works) {
+            pq.offer(work);
+        } 
+        
+        for (int i = 0; i < n; i++) {
+            if (!pq.isEmpty()) {
+                int cur = pq.poll();
+                if (cur == 1) {
+                    continue;
+                }
+                pq.offer(cur - 1);
+            }
+        }
+        
+        while (!pq.isEmpty()) {
+            int num = pq.poll();
+            answer += num * num;
+        }
+        
+        return answer;
+    }
+}
