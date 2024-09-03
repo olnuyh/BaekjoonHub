618964685c5d293962da6453433a81fbd7a22c3f
olnuyh <49039762+olnuyh@users.noreply.github.com>
2024-09-03 23:21:11 +0900

    [level 2] Title: 짝지어 제거하기, Time: 67.48 ms, Memory: 63 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution
+{
+    public int solution(String s)
+    {
+        Stack<Character> stack = new Stack();
+        
+        for (char c : s.toCharArray()) {
+            if (stack.isEmpty()) {
+                stack.push(c);
+            } else {
+                if (stack.peek() == c) {
+                    stack.pop();
+                } else {
+                    stack.push(c);
+                }
+            }
+        }
+        
+        if (stack.isEmpty()) {
+            return 1;
+        }
+
+        return 0;
+    }
+}
