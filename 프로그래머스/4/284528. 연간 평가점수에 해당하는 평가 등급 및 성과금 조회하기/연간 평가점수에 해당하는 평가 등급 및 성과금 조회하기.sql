aa06156e58baa95c17e7d7b22f4a8b2205e3d358
olnuyh <49039762+olnuyh@users.noreply.github.com>
2024-09-28 17:50:36 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -1,14 +1,15 @@
 -- 코드를 작성해주세요
-SELECT HE.EMP_NO AS EMP_NO, EMP_NAME, (CASE WHEN AVG(HG.SCORE) >= 96 THEN 'S'
-                            WHEN AVG(HG.SCORE) >= 90 THEN 'A'
-                            WHEN AVG(HG.SCORE) >= 80 THEN 'B'
-                            ELSE 'C'
-                            END) AS GRADE, 
-                            ((CASE WHEN AVG(HG.SCORE) >= 96 THEN 20
-                            WHEN AVG(HG.SCORE) >= 90 THEN 15
-                            WHEN AVG(HG.SCORE) >= 80 THEN 10
-                            ELSE 0
-                            END) * 0.01 * HE.SAL) AS BONUS
-FROM HR_EMPLOYEES AS HE INNER JOIN HR_GRADE AS HG ON HE.EMP_NO = HG.EMP_NO
-GROUP BY HE.EMP_NO
-ORDER BY HE.EMP_NO;
+SELECT HG.EMP_NO, EMP_NAME, (CASE WHEN AVG(SCORE) >= 96 THEN 'S'
+                WHEN AVG(SCORE) >= 90 THEN 'A'
+                WHEN AVG(SCORE) >= 80 THEN 'B'
+                ELSE 'C'
+                END) GRADE,
+                SAL * (CASE WHEN AVG(SCORE) >= 96 THEN 0.2
+                WHEN AVG(SCORE) >= 90 THEN 0.15
+                WHEN AVG(SCORE) >= 80 THEN 0.1
+                ELSE 0
+                END) BONUS
+FROM HR_GRADE HG INNER JOIN HR_EMPLOYEES HE
+ON HG.EMP_NO = HE.EMP_NO
+GROUP BY HG.EMP_NO
+ORDER BY HG.EMP_NO;
