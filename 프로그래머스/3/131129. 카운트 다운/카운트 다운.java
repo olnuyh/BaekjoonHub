0567e3279a68a34446f02f0207b52636c53ca6ac
olnuyh <49039762+olnuyh@users.noreply.github.com>
2024-11-21 10:40:53 +0900

    [level 3] Title: 카운트 다운, Time: 52.99 ms, Memory: 95.9 MB -BaekjoonHub

--- 프로그래머스/3/131129. 카운트 다운/카운트 다운.java
+++ 프로그래머스/3/131129. 카운트 다운/카운트 다운.java
@@ -1,46 +1,46 @@
-import java.util.Arrays;
+import java.util.*;
 
-public class Solution {
+class Solution {
     public int[] solution(int target) {
-
-        int[][] dp = new int[target + 1][2]; 
+        
+        int[][] D = new int[target + 1][2]; // 숫자 N을 만들기 위해 필요한 최소 다트 수, 최대 싱글/불 수
         
         for (int i = 1; i <= target; i++) {
-            dp[i][0] = Integer.MAX_VALUE; 
+            D[i][0] = Integer.MAX_VALUE;
         }
-
-        int[] scores = new int[62];
-        int index = 0;
+        
+        int[] scores = new int[62]; // 한 번 던져서 가능한 모든 점수
+        
+        int idx = 0;
+        
         for (int i = 1; i <= 20; i++) {
-            scores[index++] = i;
-            scores[index++] = i * 2;
-            scores[index++] = i * 3;
+            scores[idx++] = i; // 싱글
+            scores[idx++] = i * 2; // 더블
+            scores[idx++] = i * 3; // 트리플
         }
-        scores[index] = 50;
-
-        for (int i = 0; i <= target; i++) {
+        
+        scores[idx] = 50; // 불
+        
+        for (int i = 1; i <= target; i++) {
             for (int score : scores) {
                 if (i >= score) {
-
-                    int darts = dp[i - score][0] + 1;
-
-                    int singles = dp[i - score][1];
-                    if (score <= 20) {
-                        singles += 1;
-                    } else if (score == 50) {
-                        singles += 1;
+                    int dart = D[i - score][0] + 1; // 다트 수는 (i - score)를 만드는 데 사용한 다트 수 + 1
+                    int singleOrBull = D[i - score][1];
+                    
+                    if (score <= 20 || score == 50) { // 싱글이나 불 횟수를 최대화해야 하므로 해당 범위에 있는 경우 싱글이나 불로 생각
+                        singleOrBull++;
                     }
-
-                    if (darts < dp[i][0]) {
-                        dp[i][0] = darts;
-                        dp[i][1] = singles;
-                    } else if (darts == dp[i][0] && singles > dp[i][1]) {
-                        dp[i][1] = singles;
+                    
+                    if (dart < D[i][0]) { // 다트 수 최소 사용 우선
+                        D[i][0] = dart;
+                        D[i][1] = singleOrBull;
+                    } else if (dart == D[i][0] && singleOrBull > D[i][1]) { // 다트 수가 같으면 싱글/불 수 최대화
+                        D[i][1] = singleOrBull;
                     }
                 }
             }
         }
-
-        return dp[target];
+        
+        return D[target];
     }
 }
