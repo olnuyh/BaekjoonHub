330946a08cef4b7e9f5e922a51e62d2a8f5faff5
olnuyh <49039762+olnuyh@users.noreply.github.com>
2025-02-20 12:59:32 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 432 ms, Memory: 125328 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static int[] D;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] A = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int len = 0;
+        D = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            int pos = findPos(0, len, A[i]);
+
+            if (pos == -1) {
+                D[len++] = A[i];
+            } else {
+                D[pos] = A[i];
+            }
+        }
+
+        System.out.println(len);
+    }
+
+    public static int findPos (int start, int end, int val) {
+        int result = -1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (val <= D[mid]) {
+                end = mid - 1;
+                result = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return result;
+    }
+}
