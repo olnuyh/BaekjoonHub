9ead435306a3af59df12d7d22524eb77d358a49e
olnuyh <49039762+olnuyh@users.noreply.github.com>
2025-02-21 23:55:57 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 656 ms, Memory: 181468 KB -BaekjoonHub

--- 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.java
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.java
@@ -1,71 +1,77 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	static int binarySearch(int start, int end, int target) {
-		while(start<end) {
-			int mid = (start+end)/2;
-			if(lis.get(mid) == target) {
-				return mid;
-			}
-			if(lis.get(mid) < target) {
-				start = mid+1;
-			}
-			else {
-				end = mid;
-			}
-		}
-		return end;
-	}
-	
-	static class Node{
-		int v,i;
+    public static int[] D;
 
-		public Node(int v, int i) {
-			this.v = v;
-			this.i = i;
-		}
-	}
-	static List<Integer> lis = new ArrayList<>();	
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		StringTokenizer st= new StringTokenizer(br.readLine());
-		List<Node> res = new ArrayList<>();
-		StringBuilder sb = new StringBuilder();
-		lis.add(Integer.parseInt(st.nextToken()));
-		res.add(new Node(lis.get(0),0));
-		for(int i=1; i<n; i++) {
-			int next = Integer.parseInt(st.nextToken());
-			
-			if(lis.get(lis.size()-1) < next) {
-				lis.add(next);
-				res.add(new Node(next,lis.size()-1));
-			}
-			else{
-				int idx = binarySearch(0, lis.size()-1, next);
-				res.add(new Node(next,idx));
-				lis.set(idx, next);
-			}
-		}
-		sb.append(lis.size()).append("\n");
-		int idx = lis.size()-1;
-		for(int i=res.size()-1; i>=0; i--) {
-			if(res.get(i).i == idx) {
-				lis.set(idx,res.get(i).v);
-				idx--;
-			}
-		}
-		for(int i:lis) {
-			sb.append(i).append(" ");
-		}
-		System.out.println(sb);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
+        int N = Integer.parseInt(br.readLine());
+
+        int[] A = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        D = new int[N];
+        int[] order = new int[N];
+
+        D[0] = A[0];
+        int len = 1;
+
+        for (int i = 1; i < N; i++) {
+            int pos = findPos(0, len, A[i]);
+
+            if (pos == -1) {
+                D[len] = A[i];
+                order[i] = len++;
+            } else {
+                D[pos] = A[i];
+                order[i] = pos;
+            }
+        }
+
+        sb.append(len).append("\n");
+
+        int[] answer = new int[len];
+
+        len -= 1;
+
+        for (int i = N - 1; i >= 0; i--) {
+            if (order[i] == len) {
+                answer[len--] = A[i];
+            }
+        }
+
+        for (int i = 0; i < answer.length; i++) {
+            sb.append(answer[i]).append(" ");
+        }
+
+        System.out.println(sb);
+    }
+
+    public static int findPos (int start, int end, int val) {
+        int result = -1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (D[mid] >= val) {
+                end = mid - 1;
+                result = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return result;
+    }
 }
