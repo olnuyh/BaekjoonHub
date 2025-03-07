67e44a894d2847378ef11adadfc8d72c32958ba4
olnuyh <49039762+olnuyh@users.noreply.github.com>
2025-03-07 13:37:39 +0900

    [Gold V] Title: 자두나무, Time: 72 ms, Memory: 11880 KB -BaekjoonHub

---
+++ 백준/Gold/2240. 자두나무/자두나무.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int T = Integer.parseInt(st.nextToken());
+        int W = Integer.parseInt(st.nextToken());
+
+        int[] plum = new int[T + 1];
+
+        for (int i = 1; i <= T; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            plum[i] = Integer.parseInt(st.nextToken()) - 1;
+        }
+
+        int[][][] D = new int[T + 1][2][W + 1];
+
+        for (int i = 0; i <= T; i++) {
+            for (int j = 0; j < 2; j++) {
+                Arrays.fill(D[i][j], -1);
+            }
+        }
+
+        D[0][0][0] = 0;
+
+        for (int i = 1; i <= T; i++) {
+            for (int j = 0; j < 2; j++) {
+                int val = plum[i] == j? 1 : 0;
+
+                for (int k = 0; k <= W; k++) {
+                    if (D[i - 1][j][k] != -1) {
+                        D[i][j][k] = D[i - 1][j][k] + val;
+                    }
+                }
+
+                for (int k = 1; k <= W; k++) {
+                    if (D[i - 1][(j + 1) % 2][k - 1] != -1) {
+                        D[i][j][k] = Math.max(D[i][j][k], D[i - 1][(j + 1) % 2][k - 1] + val);
+                    }
+                }
+            }
+        }
+
+        int max = 0;
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j <= W; j++) {
+                max = Math.max(max, D[T][i][j]);
+            }
+        }
+
+        System.out.println(max);
+    }
+}
