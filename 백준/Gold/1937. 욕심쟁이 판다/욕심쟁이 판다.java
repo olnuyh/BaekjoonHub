d9c568a3dd113720029cb974b369057cb1728b91
olnuyh <49039762+olnuyh@users.noreply.github.com>
2025-03-25 23:51:31 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 380 ms, Memory: 40604 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
@@ -4,62 +4,61 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static int n;
-	public static int[][] forest;
-	public static int[][] visited;
-	public static int[] dr = {-1, 1, 0, 0};
-	public static int[] dc = {0, 0, -1, 1};
+    public static int N;
+    public static int[][] forest;
+    public static int[][] deltas = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+    public static int[][] D;
+    public static int maxCount;
 
-	public static int dfs(int r, int c) {
-		int cnt = 0;
-		for(int i = 0; i < 4; i++) {
-			int nr = r + dr[i];
-			int nc = c + dc[i];
-			
-			if(nr < 0 || nr >= n || nc < 0 || nc >= n)
-				continue;
-			
-			if(forest[nr][nc] > forest[r][c]) {
-				if(visited[nr][nc] == 0)
-					cnt = Math.max(cnt, dfs(nr, nc));
-				else
-					cnt = Math.max(cnt, visited[nr][nc]);
-			}
-		}
-		
-		visited[r][c] = cnt + 1;
-		return visited[r][c];
-	}
-	
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		n = Integer.parseInt(br.readLine());
-		
-		forest = new int[n][n];
-		
-		for(int i = 0; i < n; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j = 0; j < n; j++)
-				forest[i][j] = Integer.parseInt(st.nextToken());
-		}
-		
-		visited = new int[n][n];
-		int answer = 0;
-		
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < n; j++) {
-				if(visited[i][j] == 0) {
-					dfs(i, j);
-				}
-				
-				if(answer < visited[i][j])
-					answer = visited[i][j];
-			}
-		}
-		
-		System.out.println(answer);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
+        N = Integer.parseInt(br.readLine());
+
+        forest = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            for (int j = 0; j < N; j++) {
+                forest[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        D = new int[N][N];
+
+        maxCount = 0;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                maxCount = Math.max(maxCount, move(i, j));
+            }
+        }
+
+        System.out.println(maxCount);
+    }
+
+    public static int move (int r, int c) {
+        if (D[r][c] != 0) {
+            return D[r][c];
+        }
+
+        D[r][c] = 1;
+
+        for (int d = 0; d < 4; d++) {
+            int nr = r + deltas[d][0];
+            int nc = c + deltas[d][1];
+
+            if (isIn(nr, nc) && forest[r][c] < forest[nr][nc]) {
+                D[r][c] = Math.max(D[r][c], move(nr, nc) + 1);
+            }
+        }
+
+        return D[r][c];
+    }
+
+    public static boolean isIn (int r, int c) {
+        return r >= 0 && r < N && c >= 0 && c < N;
+    }
 }
