3a16e0bf7caf8c849030eafd040a832ff4b95110
olnuyh <49039762+olnuyh@users.noreply.github.com>
2025-03-29 12:21:56 +0900

    [Silver III] Title: N과 M (1), Time: 148 ms, Memory: 20576 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -4,32 +4,37 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+    public static boolean[] visited;
+    public static int N;
     public static int[] nums;
-    public static int N,M;
     public static StringBuilder sb;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        sb = new StringBuilder();
 
         N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
         nums = new int[N];
+
         for (int i = 0; i < N; i++) {
             nums[i] = i + 1;
         }
 
-        makeNums(0, new int[M], new boolean[N]);
+        visited = new boolean[N];
+        sb = new StringBuilder();
+
+        permutation(0, M, new int[M]);
 
         System.out.println(sb);
     }
 
-    public static void makeNums(int count, int[] selected, boolean[] visited) {
-        if (count == M) {
-            for (int i = 0; i < M; i++) {
-                sb.append(selected[i]).append(" ");
+    public static void permutation (int depth, int M, int[] selected) {
+        if (depth == M) {
+
+            for (int num : selected) {
+                sb.append(num).append(" ");
             }
 
             sb.append("\n");
@@ -40,8 +45,8 @@ public class Main {
         for (int i = 0; i < N; i++) {
             if (!visited[i]) {
                 visited[i] = true;
-                selected[count] = nums[i];
-                makeNums(count + 1, selected, visited);
+                selected[depth] = nums[i];
+                permutation(depth + 1, M, selected);
                 visited[i] = false;
             }
         }
