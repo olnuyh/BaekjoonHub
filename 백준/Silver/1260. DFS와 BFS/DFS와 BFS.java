23944152f7da14a8258db5a6bacdebde56580ee8
olnuyh <49039762+olnuyh@users.noreply.github.com>
2025-03-29 12:16:16 +0900

    [Silver II] Title: DFS와 BFS, Time: 156 ms, Memory: 17456 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,79 +1,81 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-	public static boolean[] visited;
-	public static StringBuilder sb;
-	public static ArrayList<Integer>[] list;
-	
-	public static void dfs(int num) {
-		visited[num] = true;
-		sb.append(num + " ");
-		
-		for(int i = 0; i < list[num].size(); i++) {
-			int next = list[num].get(i);
-			if(!visited[next])
-				dfs(next);
-		}
-	}
-	
-	public static void bfs(int num) {
-		Queue<Integer> q = new ArrayDeque<>();
-		q.add(num);
-		visited[num] = true;
-		
-		while(!q.isEmpty()) {
-			int v = q.poll();
-			sb.append(v + " ");
-			for(int i = 0; i < list[v].size(); i++) {
-				int next = list[v].get(i);
-				if(!visited[next]) {
-					q.add(next);
-					visited[next] = true;
-				}
-			}
-		}
-	}
+    public static List<Integer>[] graph;
+    public static boolean[] visited;
+    public static StringBuilder sb;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		sb = new StringBuilder();
-		
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		int v = Integer.parseInt(st.nextToken());
-		
-		list = new ArrayList[n + 1];
-		
-		for(int i = 1; i <= n; i++)
-			list[i] = new ArrayList<>();
-		
-		for(int i = 0; i < m; i++) {
-			st = new StringTokenizer(br.readLine());
-			int s = Integer.parseInt(st.nextToken());
-			int e = Integer.parseInt(st.nextToken());
-			
-			list[s].add(e);
-			list[e].add(s);
-		}
-		
-		for(int i = 1; i <= n; i++)
-			Collections.sort(list[i]);
-		
-		visited = new boolean[n + 1];
-		dfs(v);
-		sb.append("\n");
-		visited = new boolean[n + 1];
-		bfs(v);
-		
-		System.out.println(sb);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+
+        graph = new List[N + 1];
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            graph[a].add(b);
+            graph[b].add(a);
+        }
+
+        for (int i = 1; i <= N; i++) {
+            Collections.sort(graph[i]);
+        }
+
+        sb = new StringBuilder();
+
+        visited = new boolean[N + 1];
+        dfs(V);
+        sb.append("\n");
+
+        visited = new boolean[N + 1];
+        bfs(V);
+        sb.append("\n");
+
+        System.out.println(sb);
+    }
+
+    public static void dfs (int num) {
+        visited[num] = true;
+        sb.append(num).append(" ");
+
+        for (int next : graph[num]) {
+            if (!visited[next]) {
+                dfs(next);
+            }
+        }
+    }
+
+    public static void bfs (int num) {
+        Queue<Integer> q = new ArrayDeque<>();
+        q.offer(num);
+
+        visited[num] = true;
+
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+
+            sb.append(cur).append(" ");
+
+            for (int next : graph[cur]) {
+                if (!visited[next]) {
+                    visited[next] = true;
+                    q.offer(next);
+                }
+            }
+        }
+    }
 }
