7b1a78bac063de58d5d532e29b5e3b9a0b67feb9
olnuyh <49039762+olnuyh@users.noreply.github.com>
2025-04-21 21:03:32 +0900

    [Gold V] Title: 배열 돌리기 2, Time: 488 ms, Memory: 32592 KB -BaekjoonHub

--- 백준/Gold/16927. 배열 돌리기 2/배열 돌리기 2.java
+++ 백준/Gold/16927. 배열 돌리기 2/배열 돌리기 2.java
@@ -4,74 +4,73 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static int[][] arr;
-	public static int n, m, r;
-	
-	public static int[] dr = {0, 1, 0, -1};
-	public static int[] dc = {1, 0, -1, 0};
+    public static int N, M;
+    public static int[][] arr;
+    public static int[][] deltas = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
-		
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		r = Integer.parseInt(st.nextToken());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
-		arr = new int[n][m];
-		for(int i = 0; i < n; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j = 0; j < m; j++)
-				arr[i][j] = Integer.parseInt(st.nextToken());
-		}
-		
-		int nowN = n;
-		int nowM = m;
-		
-		for(int i = 0; i < Math.min(n, m) / 2; i++) {
-			
-			rotation(i, 2 * (nowN + nowM - 2));
-			nowN -= 2;
-			nowM -= 2;
-		}
-		
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < m; j++)
-				sb.append(arr[i][j] + " ");
-			sb.append("\n");
-		}
-		
-		System.out.println(sb);
-	}
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
 
-	public static void rotation(int start, int len) {
-		int k = r % len;
-		
-		for(int i = 0; i < k; i++) {
-			int temp = arr[start][start];
-			int curR = start;
-			int curC = start;
-			int d = 0;
-			
-			while(d < 4) {
-				int nr = curR + dr[d];
-				int nc = curC + dc[d];
-				
-				if(nr == start && nc == start)
-					break;
-				
-				if(nr < start || nr >= n - start || nc < start || nc >= m - start) {
-					d++;
-					continue;
-				}
-				
-				arr[curR][curC] = arr[nr][nc];
-				curR = nr;
-				curC = nc;
-			}
-			
-			arr[start + 1][start] = temp;
-		}
-	}
+        arr = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            for (int j = 0; j < M; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        rotate(R);
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                sb.append(arr[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    public static void rotate (int R) {
+        int cnt = Math.min(N, M) / 2;
+
+        for (int i = 0; i < cnt; i++) {
+            int move = R % (((N - i * 2) + (M - i * 2)) * 2 - 4);
+
+            for (int j = 0; j < move; j++) {
+                int r = i;
+                int c = i;
+                int d = 0;
+
+                int temp = arr[r][c];
+
+                while (d < 4) {
+                    int nr = r + deltas[d][0];
+                    int nc = c + deltas[d][1];
+
+                    if (nr >= i && nr < N - i && nc >= i && nc < M - i) {
+                        arr[r][c] = arr[nr][nc];
+                        r = nr;
+                        c = nc;
+                    } else {
+                        d++;
+                    }
+                }
+
+                arr[i + 1][i] = temp;
+            }
+        }
+    }
+
+    public static boolean isIn (int r, int c) {
+        return r >= 0 && r < N && c >= 0 && c < M;
+    }
 }
