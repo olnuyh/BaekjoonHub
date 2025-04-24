2872789eab152711e1fa57ed74bec01501db7433
olnuyh <49039762+olnuyh@users.noreply.github.com>
2025-04-24 13:00:38 +0900

    [Silver V] Title: 🐜 기적의 매매법 🐜, Time: 68 ms, Memory: 11532 KB -BaekjoonHub

---
+++ 백준/Silver/20546. 🐜 기적의 매매법 🐜/🐜 기적의 매매법 🐜.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int money = Integer.parseInt(br.readLine());
+        int junhyeonMoney = money;
+        int sungminMoney = money;
+
+        int[] price = new int[15];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= 14; i++) {
+            price[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int junhyeonStock = 0;
+        int sungminStock = 0;
+
+        for (int i = 1; i <= 14; i++) {
+            if (junhyeonMoney >= price[i]) {
+                junhyeonStock += junhyeonMoney / price[i];
+                junhyeonMoney = junhyeonMoney % price[i];
+            }
+
+            if (i - 3 >= 0) {
+                if (price[i - 3] < price[i - 2] && price[i - 2] < price[i - 1]) {
+                    sungminMoney += sungminStock * price[i];
+                    sungminStock = 0;
+                } else if (price[i - 3] > price[i - 2] && price[i - 2] > price[i - 1] && sungminMoney >= price[i]) {
+                    sungminStock += sungminMoney / price[i];
+                    sungminMoney = sungminMoney % price[i];
+                }
+            }
+        }
+
+        if (junhyeonMoney + junhyeonStock * price[14] > sungminMoney + sungminStock * price[14]) {
+            System.out.println("BNP");
+        } else if (junhyeonMoney + junhyeonStock * price[14] < sungminMoney + sungminStock * price[14]) {
+            System.out.println("TIMING");
+        } else {
+            System.out.println("SAMESAME");
+        }
+    }
+}
